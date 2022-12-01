6833fe51e76ff5e258b32888bfef042641220d02
Valeria Serci <balerty84@gmail.com>
2022-12-01 11:42:09 +0000

    add 3 players with 3 properties each

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const team = {
-    _players: [],
+    _players: [
+      {firstName: 'Paul', lastname: 'Smith', age: 36},
+      {firstName: 'Bob', lastname: 'Sloan', age: 38},
+      {firstName: 'Tom', lastname: 'Thor', age: 32}
+    ],
     _games: []
   };
